703f0078b4bfa076d87f308d6df3ce95d3b93aa8
mati <matizieba@gmail.com>
2022-03-03 14:58:13 +0100

    email sender

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect,url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_ckeditor import CKEditorField
+import os
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+import smtplib
+
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+bootstrap = Bootstrap(app)
+ckeditor = CKEditor(app)
+
+
+class ContactForm(FlaskForm):
+    title = StringField("Message topic", validators=[DataRequired()])
+    email = StringField("Your email", validators=[DataRequired(), Email()])
+    body = CKEditorField("Your message", validators=[DataRequired()])
+    submit = SubmitField("Sent")
+
+SECRET_KEY = "76736jfmsdkgsadmk53953uasfa@#$%%"
 
 @app.route("/")
 def main():
@@ -16,6 +38,28 @@ def manager():
 def developer():
     is_main = False
     return render_template('developer.html', is_main=is_main)
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        message = {
+            "title":form.title.data,
+            "email":form.email.data,
+            "message":form.body.data,
+        }
+        my_email = "xxx"
+        password = "xxx"
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs="matizieba@gmail.com",
+                                msg=message['message'],)
+            return redirect(url_for('main'))
+    is_main = False
+    return render_template('contact.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

3903f7e54267dfef29b5532d3a64a0d7ad258bf8
mati <matizieba@gmail.com>
2022-03-03 12:07:56 +0100

    navbar sticky + manager + dev section

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-     return render_template('index.html')
+def main():
+    is_main = True
+    return render_template('index.html', is_main=is_main)
+
+@app.route("/manager")
+def manager():
+    is_main = False
+    return render_template('manager.html', is_main=is_main)
+
+@app.route("/developer")
+def developer():
+    is_main = False
+    return render_template('developer.html', is_main=is_main)
 
 if __name__ == "__main__":
     app.run(debug=True)
